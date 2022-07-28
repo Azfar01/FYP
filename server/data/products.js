e76d4be0eb39657de1366bc65ae49119a8faa9be
Azfar01 <92525685+Azfar01@users.noreply.github.com>
2022-07-28 11:49:00 +0500

    First commit

---
+++ server/data/products.js
@@ -0,0 +1,65 @@
+const products = [
+	{
+		name: 'Jasmine white',
+		image: '/images/p1.jpeg',
+		description:
+			'Kiladar frock with tilla embroidery and button details.',
+		brand: 'Safayd',
+		category: ' SUMMER COLLECTION',
+		price: 2999,
+		countInStock: 10,
+		rating: 4.5,
+		numReviews: 12,
+	},
+	{
+		name: 'Jasmine white lite',
+		image: '/images/p2.jpg',
+		description:
+			'Kiladar frock with tilla embroidery and button details.',
+		brand: 'Safayd',
+		category: 'SUMMER COLLECTION',
+		price: 2890,
+		countInStock: 7,
+		rating: 4.0,
+		numReviews: 8,
+	},
+	{
+		name: 'Jasmine white lite',
+		image: '/images/p2.jpg',
+		description:
+			'Kiladar frock with tilla embroidery and button details.',
+		brand: 'Safayd',
+		category: 'SUMMER COLLECTION',
+		price: 2890,
+		countInStock: 7,
+		rating: 4.0,
+		numReviews: 8,
+	},
+	{
+		name: 'Jasmine white lite',
+		image: '/images/p2.jpg',
+		description:
+			'Kiladar frock with tilla embroidery and button details.',
+		brand: 'Safayd',
+		category: 'SUMMER COLLECTION',
+		price: 2890,
+		countInStock: 7,
+		rating: 4.0,
+		numReviews: 8,
+	},
+	{
+		name: 'Jasmine white',
+		image: '/images/p1.jpeg',
+		description:
+			'Kiladar frock with tilla embroidery and button details.',
+		brand: 'Safayd',
+		category: ' SUMMER COLLECTION',
+		price: 2999,
+		countInStock: 10,
+		rating: 4.5,
+		numReviews: 12,
+	},
+
+];
+
+export default products;
